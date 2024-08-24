e9526d03f9fd24bf01497506edf4534aad7aec37
rumanaamir <rumanaamir2022@gmail.com>
2024-08-24 13:23:53 +0000

    Q6

--- Q5.py
+++ Q5.py
@@ -4,6 +4,6 @@ celsius = int(input("Enter the temperature in celsius: "));
 fahren_cel = (fahrenheit - 32) * 5//9;
 cel_fahren = (celsius * 9/5) + 32;
 
-print("/nAfter conversion");
+print("\nAfter conversion");
 print("Celsius: ",fahren_cel,"C");
 print("Fahreenheit: ",cel_fahren,"F");
---
+++ Q6.py
@@ -0,0 +1,6 @@
+seconds = int(input("Enter the number of seconds: "));
+
+min = seconds // 60;
+sec = seconds % 60;
+
+print(min,"min", sec, "sec");
