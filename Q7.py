35140c8ac76bbcc1681f62e8b0305e6273f59420
rumanaamir <rumanaamir2022@gmail.com>
2024-08-24 18:43:48 +0000

    Q7

---
+++ Q7.py
@@ -0,0 +1,7 @@
+numerator =  int(input(" Enter the value of numerator: "));
+
+denominator = int(input(" Enter the value of denominator: "));
+
+pct: int = (numerator / denominator) * 100;
+
+print("percentage: ",pct,"%");
---
+++ Q9.py
@@ -0,0 +1,9 @@
+pi =  float( input (" Enter the value of pi: "));
+
+radius= float( input (" Enter the value of radius: "));
+
+height= float(input ("Enter the value of height: "));
+
+volume:  float= pi * radius**2 * height ;
+
+print(" volume of cylinder : " ,volume);
