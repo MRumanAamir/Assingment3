68d1665678cc78334f26b072de81236192ac290d
rumanaamir <rumanaamir2022@gmail.com>
2024-08-24 18:55:15 +0000

    Q9

--- Q9.py
+++ Q9.py
@@ -6,4 +6,6 @@ height= float(input ("Enter the value of height: "));
 
 volume:  float= pi * radius**2 * height ;
 
-print(" volume of cylinder : " ,volume);
+print(" volume of cylinder : " ,volume); 
+
+
